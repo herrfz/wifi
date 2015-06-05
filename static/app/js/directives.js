57adc08d56e08f6603027ebe0eaee81a7c48b7ef
Eriza Fazli <erizzaaaaa@gmail.com>
2015-06-05 19:12:09 +0200

    added license

--- static/app/js/directives.js
+++ static/app/js/directives.js
@@ -1,3 +1,10 @@
+/*!
+ *
+ * Copyright 2015, Eriza Fazli <erizzaaaaa@gmail.com> 
+ * Licensed under the GPL Version 3 license.
+ * https://github.com/herrfz/LICENSE
+ *
+ */
 'use strict';
 
 /* Directives */
