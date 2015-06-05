46c338ba4ad58de56b04d6d192a523e376afb663
Eriza Fazli <erizzaaaaa@gmail.com>
2015-06-05 19:19:50 +0200

    fixed license

--- static/app/js/app.js
+++ static/app/js/app.js
@@ -2,7 +2,7 @@
  *
  * Copyright 2015, Eriza Fazli <erizzaaaaa@gmail.com> 
  * Licensed under the GPL Version 3 license.
- * https://github.com/herrfz/LICENSE
+ * https://github.com/herrfz/wifi/LICENSE
  *
  */
 'use strict';
